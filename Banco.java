ded84b0a555f02d142ceac9cd21d54e19813fc45
Gabriel Oliveira <74208625+gaabrieloliver@users.noreply.github.com>
2022-03-20 17:15:40 -0300

    Em andamento.

---
+++ Banco.java
@@ -0,0 +1,13 @@
+public class Banco {
+
+    private String nome;
+
+    //ALT+Insert gerei Getter and Setters.
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
